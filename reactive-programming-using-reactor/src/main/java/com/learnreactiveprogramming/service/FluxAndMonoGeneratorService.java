9ce3ad184463a4a0dffeba56cc82c3599c91b281
Ahmed Yehia <a.yehia@boutiqaat.com>
2023-02-05 12:53:39 +0300

    lesson: 14 - Let's write our very first Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,8 +1,10 @@
 package com.learnreactiveprogramming.service;
 
 import java.util.List;
+import java.util.function.Consumer;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 public class FluxAndMonoGeneratorService {
@@ -12,9 +14,20 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("John", "Ahmed", "Dilip"));
     }
 
+    public Mono<String> nameMono() {
+
+        return Mono.just("Ahmed");
+    }
+
     public static void main(String[] args) {
 
+        final Consumer<String> logger = name -> log.info("Name is: " + name);
         final var service = new FluxAndMonoGeneratorService();
-        service.namesFlux().subscribe(name -> log.info("Name is: " + name));
+        log.trace("Subscribing to namesFlux.");
+        service.namesFlux().subscribe(logger);
+        log.trace("namesFlux consumed.");
+        log.trace("Subscribing to nameMono.");
+        service.nameMono().subscribe(logger);
+        log.trace("nameMono consumed.");
     }
 }
