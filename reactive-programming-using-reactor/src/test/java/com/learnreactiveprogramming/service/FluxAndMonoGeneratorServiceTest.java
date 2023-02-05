1495831fa11d9c144d0946ed6f24f9744d128427
Ahmed Yehia <a.yehia@boutiqaat.com>
2023-02-05 14:26:32 +0300

    lesson: 16 - Testing Flux using JUnit5

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,19 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        final var namesFlux = service.namesFlux();
+        StepVerifier.create(namesFlux)
+                .expectNext("John")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 }
