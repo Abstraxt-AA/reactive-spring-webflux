fcd8d0181eed9f728333cbd4c2b2afcc8ab45615
Ahmed Yehia <a.yehia@boutiqaat.com>
2023-02-09 11:15:34 +0300

    lesson: 38 - Build Infinite Streams API (SSE)

--- movies-info-service/src/main/java/moviesinfoservice/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/moviesinfoservice/controller/FluxAndMonoController.java
@@ -1,5 +1,7 @@
 package moviesinfoservice.controller;
 
+import java.time.Duration;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -17,5 +19,10 @@ public class FluxAndMonoController {
     @GetMapping("/mono")
     public Mono<Integer> getIntMono() {
         return Mono.just(1).log();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> streamLong() {
+        return Flux.interval(Duration.ofSeconds(1)).log();
     }
 }
