24e775dec5285b24cdf1403a2668e1a1a79a7912
Ahmed Yehia <a.yehia@boutiqaat.com>
2023-02-09 11:53:58 +0300

    lesson: 41 - Different Approaches of Testing an Endpoint

--- movies-info-service/src/test/java/unit/com/reactivespring/moviesinfoservice/controller/FluxAndMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/moviesinfoservice/controller/FluxAndMonoControllerTest.java
@@ -1,10 +1,12 @@
 package com.reactivespring.moviesinfoservice.controller;
 
+import java.util.Objects;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
 
 @WebFluxTest(controllers = FluxAndMonoController.class)
 @AutoConfigureWebTestClient
@@ -23,6 +25,39 @@ class FluxAndMonoControllerTest {
                 .is2xxSuccessful()
                 .expectBodyList(Integer.class)
                 .hasSize(3);
+    }
+
+    @Test
+    void getIntFlux_SecondApproach() {
+
+        final var response = client.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+        StepVerifier.create(response)
+                .expectNext(1, 2, 3)
+                .verifyComplete();
+    }
+
+    @Test
+    void getIntFlux_ThirdApproach() {
+
+        final var response = client.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(result -> {
+                    final var body = result.getResponseBody();
+                    assert (Objects.requireNonNull(body).size()) == 3;
+                    for (int i = 0; i < 3; i++) {
+                        assert body.get(i) == i + 1;
+                    }
+                });
     }
 
     @Test
