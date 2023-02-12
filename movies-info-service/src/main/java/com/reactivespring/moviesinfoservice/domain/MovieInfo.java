bd7b2fd8720f87e2fc233fecc647048fc626dc53
Ahmed Yehia <a.yehia@boutiqaat.com>
2023-02-12 09:28:36 +0300

    lesson: 44 - Set up the MovieInfo Document

---
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/MovieInfo.java
@@ -0,0 +1,26 @@
+package com.reactivespring.moviesinfoservice.domain;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MovieInfo {
+
+    @Id
+    private Integer movieInfoId;
+    private String name;
+    private Integer year;
+    private List<String> cast;
+    private LocalDate releaseDate;
+}
