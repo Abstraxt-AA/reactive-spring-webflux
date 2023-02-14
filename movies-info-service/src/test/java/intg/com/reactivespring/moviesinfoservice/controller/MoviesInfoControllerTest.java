9c2cdb8551d6d2a99a72b9ec4d58a43bc3face70
Ahmed Yehia <a.yehia@boutiqaat.com>
2023-02-14 10:33:05 +0300

    lesson: 54 - Integration Test for the POST end point using JUnit5

---
+++ movies-info-service/src/test/java/intg/com/reactivespring/moviesinfoservice/controller/MoviesInfoControllerTest.java
@@ -0,0 +1,64 @@
+package com.reactivespring.moviesinfoservice.controller;
+
+import com.reactivespring.moviesinfoservice.domain.MovieInfo;
+import com.reactivespring.moviesinfoservice.repository.MovieInfoRepository;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class MoviesInfoControllerTest {
+
+    private final MovieInfoRepository repository;
+    private final WebTestClient webTestClient;
+    private static final String MOVIES_INFO_URL = "/v1/movie-infos";
+
+    public MoviesInfoControllerTest(@Autowired final MovieInfoRepository repository,
+            @Autowired final WebTestClient webTestClient) {
+        this.repository = repository;
+        this.webTestClient = webTestClient;
+    }
+
+    @BeforeEach
+    void setUp() {
+        final var infos = List.of(new MovieInfo(null, "Batman Begins", 2005, List.of("Chirstian Bale", "Michael Cane"),
+                        LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight", 2008, List.of("Chirstian Bale", "Heath Ledger"),
+                        LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises", 2012, List.of("Chirstian Bale", "Michael Cane"),
+                        LocalDate.parse("2012-07-20")));
+
+        repository.saveAll(infos).blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll().block();
+    }
+
+    @Test
+    void addMovieInfo() {
+        webTestClient
+                .post()
+                .uri(MOVIES_INFO_URL)
+                .bodyValue(new MovieInfo(null, "Batman Begins1", 2005, List.of("Chirstian Bale", "Michael Cane"),
+                        LocalDate.parse("2005-06-15")))
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(MovieInfo.class)
+                .consumeWith(result -> {
+                    final var savedMovieInfo = result.getResponseBody();
+                    assert savedMovieInfo != null;
+                    assert savedMovieInfo.getMovieInfoId() != null;
+                });
+    }
+}
--- movies-info-service/src/test/java/intg/com/reactivespring/moviesinfoservice/repository/MovieInfoRepositoryIntegrationTest.java
+++ movies-info-service/src/test/java/intg/com/reactivespring/moviesinfoservice/repository/MovieInfoRepositoryIntegrationTest.java
@@ -10,15 +10,12 @@ import java.util.Objects;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.test.StepVerifier;
 
 @ActiveProfiles("test")
-@ExtendWith(SpringExtension.class)
 @DataMongoTest(excludeAutoConfiguration =
         org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration.class)
 class MovieInfoRepositoryIntegrationTest {
